092a4d6bde089b2423840e7e69501dd0df418400
難波隼人 <hayatonanba@gmail.com>
2025-05-23 18:52:24 +0900

    fix: text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 export default function Page() {
-	return <div className='flex justify-center border-2 p-1.5'>Hello World</div>;
+	return <div className='flex justify-center border-2 p-1.5'>Hello World!!!</div>;
 }
 
 //TODO: 修正
